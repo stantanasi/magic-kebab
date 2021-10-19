67a87591aa57d9c95a64ca5e18ecb551a8dfd3a2
LoryStanTanasi <lory-stan.tanasi@42c.fr>
2021-10-19 14:38:31 +0200

    Footer is a footer HTML element

--- src/components/footer/footer.styled.ts
+++ src/components/footer/footer.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const FooterWrapper = styled.div`
+export const FooterWrapper = styled.footer`
   margin-top: 5rem;
   /* background: linear-gradient(180deg, rgba(77, 3, 2, 0) 0%, #4D0302 100%); */
 `;
