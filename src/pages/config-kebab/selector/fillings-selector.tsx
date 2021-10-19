4d4f0521915cb99f72e2d46064202b6fcfbf8630
LoryStanTanasi <lory-stan.tanasi@42c.fr>
2021-10-19 14:12:54 +0200

    Add Fillings selector events

--- src/pages/config-kebab/selector/fillings-selector.tsx
+++ src/pages/config-kebab/selector/fillings-selector.tsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
+import Button from '../../../components/common/button';
+import Element from '../../../components/common/element';
 import fillings from '../../../data/fillings.data'
+import * as Styled from './selector.styled';
 
 interface Props {
-  
+
 }
 
 const FillingsSelector = (props: Props) => {
+  const history = useHistory();
+  const [selectedFillings, setSelectedFillings] = useState([] as string[]);
+
   return (
-    <div>
-      <h2>Salade, tomate, oignons ?</h2>
-      {fillings.map(filling => (
-        <div>
-          <p>{filling.name}</p>
-          <img src={filling.image} alt={filling.name} />
-        </div>
-      ))}
-    </div>
+    <Styled.SelectorWrapper>
+      <h1>Salade, tomate, oignons ?</h1>
+
+      <Styled.ElementWrapper>
+        {fillings.map((filling, index) => (
+          <Element
+            key={index}
+            index={index}
+            title={filling.name}
+            image={filling.image}
+            isSelected={selectedFillings.includes(filling.slug)}
+            onSelect={(isSelected) => setSelectedFillings(isSelected ? selectedFillings.concat(filling.slug) : selectedFillings.filter(f => f !== filling.slug))} />
+        ))}
+      </Styled.ElementWrapper>
+
+      <Button
+        name="Continuer"
+        onClick={() => history.push("/config-kebab/sauces")} />
+
+    </Styled.SelectorWrapper>
   )
 }
 
